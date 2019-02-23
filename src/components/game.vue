ee70d289bee79602f5f31a6ff58c186911102fa6
Akshat, Akshat (US - Mumbai) <aakshat@deloitte.com>
2019-02-24 01:08:04 +0530

    Removed hello-world Added game

---
+++ src/components/game.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="game-container">
+        <canvas ref="gameArea"></canvas>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'game',
+    data() {
+        return {
+            gameArea: '',
+            gameCanvasContext: '',
+        }
+    },
+    methods: {
+        setupGamingArea() {
+            this.gameArea = this.$refs.gameArea;
+            this.gameCanvasContext = this.gameArea.getContext('2d');
+        }
+    },
+    mounted() {
+        this.setupGamingArea();
+    },
+}
+</script>
+
+<style>
+
+</style>
+
